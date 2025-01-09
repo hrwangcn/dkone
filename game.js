1dddbae3d5d892f5796b6f783f1939e08b289352
hrwangcn@win10.pc <hrwangaqsdcn@foxmail.com>
2025-01-09 22:45:45 +0800

    提取了打分表

--- game.js
+++ game.js
@@ -3,56 +3,6 @@ class Player {
     static get EMPTY() { return 0; }
     static get WHITE() { return -1; }
 
-    static get VALUETABLE() {
-        return {
-            "aaao":-10,
-            "bbbo": 10,
-            "aaaa": 1000,
-            "bbbb": -1000,
-            "aaba": 20,
-            "abaa": 20,
-            "bbab": -20,
-            "babb": -20,
-            "aaoa": 100,
-            "aoaa": 100,
-            "bbob": -100,
-            "bobb": -100,
-            "aaab": 20,
-            "baaa": 20,
-            "abbb": -20,
-            "bbba": -20,
-            "bbao": 10,
-            "bboa": 10,
-            "oabb": 10,
-            "aobb": 10,
-            "abbo": 10,
-            "obba": 10,
-            "aabo": -10,
-            "obaa": -10,
-            "boaa": -10,
-            "aaob": -10,
-            "baao": -10,
-            "oaab": -10,
-            "abao": 10,
-            "oaba": 10,
-            "obab": -10,
-            "babo": -10,
-            "aaoo": 20,
-            "ooaa": 20,
-            "aooa": 20,
-            "bboo": -20,
-            "boob": -20,
-            "aoao": -30,
-            "bobo": 30,
-            "oaoa": -30,
-            "obob": 30,
-            "aooo": 10,
-            "oooa": 10,
-            "booo": -10,
-            "ooob": 10
-        }
-    }
-
     constructor(type, isAI) {
         this.type = type;
         this.isAI = isAI;
@@ -138,7 +88,7 @@ class Player {
 
     evaluateLine(line) {
         let lineString = line.getCharacterString(this.type);
-        return Player.VALUETABLE[lineString] || 0;
+        return VALUETABLE[lineString] || 0;
     }
 }
 
